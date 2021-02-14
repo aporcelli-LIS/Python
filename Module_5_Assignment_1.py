6f2836fb77846908270c1105d86a9ddd121972cf
Angela Porcelli <77314532+aporcelli-LIS@users.noreply.github.com>
2021-02-13 19:04:26 -0500

    I cleaned up the code, removing unneeded lines, and reformatted my final print line so that the printed strings resembled a single sentence format instead of a separate line for each element.

--- Module_5_Assignment_1.py
+++ Module_5_Assignment_1.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from typing import Tuple
 
 
@@ -11,3 +12,13 @@ print(y)
 print(insert_string_middle('',x))
 print(insert_string_middle('','will be fostered for'))
 print(insert_string_middle('',y))
+=======
+def insert_string_middle(str,word):
+    return str[:2] + word + str[2:] 
+print("Enter foster name")
+x=input()
+print(x)
+y=input('Enter length of time')
+print(y)
+print(insert_string_middle('',x),'will be fostered for',y )
+>>>>>>> Stashed changes
