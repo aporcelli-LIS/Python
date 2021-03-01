c41c93edaa550ddb8d7a24ab5909057a8f72824e
Angela Porcelli <77314532+aporcelli-LIS@users.noreply.github.com>
2021-03-01 02:48:54 -0500

    Add files via upload
    Create a timedelta object representing 100 days, 10 hours, and 13 minutes.

---
+++ Mod_7_3.py
@@ -0,0 +1,14 @@
+class PA: 
+     
+    days = "100 days"
+    hours = "10 hours"
+    minutes = "13 minutes"
+   
+    def d(self): 
+        print(self.days)
+        print(self.hours)
+        print(self.minutes) 
+ 
+Date = PA()
+ 
+print(Date.days,Date.hours,Date.minutes)
