3ec322b82eb3530944c77100553cb016c058aeb8
Angela Porcelli <77314532+aporcelli-LIS@users.noreply.github.com>
2021-03-01 03:58:15 -0500

    Write a function that takes two arguments (feet and inches) with this time object

---
+++ Mod_7_4.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+now = datetime.now()
+print(now)
+def LQ(arguments, *argument2):
+    print(arg1)
+    for arg in argument2:
+        print(arg2)
+arg1 = '15840 feet'
+arg2 = '6336 inches' 
+
+print("As of", now, "I have walked", arg1,"and", arg2)
