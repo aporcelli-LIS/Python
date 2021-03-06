3f0be412f1aa3c89e519fb3a51b4324cb6873280
Angela Porcelli <77314532+aporcelli-LIS@users.noreply.github.com>
2021-03-06 00:46:00 -0500

    Updates to clarifications in Soft Copy code

--- Mod_8_SoftCopy.py
+++ Mod_8_SoftCopy.py
@@ -4,14 +4,14 @@ foster_pets = [['Squeak', 'Siracha'], ['Tilly', 'Andy']]
 #2020 pets fostered from SPCA
 
 foster_pocketpets= copy.copy(foster_pets)
-#Soft copy to alter original foster_pets list
+#Soft copy of original foster_pets list
 
 foster_pets[1][1] = ['Bun Bun']
-#Remove 'Tilly' from the original foster_pet list
-
+#Replace the name 'Andy' to 'Bun Bun' in the original foster_pet list
 
 print(foster_pets)
 print(foster_pocketpets) 
 
 x=foster_pocketpets
-print(x)
+#Test to ensure soft copy properties
+print(x) 
