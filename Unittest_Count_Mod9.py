9cabbf3fb7097f50c893da398d33969dce95df2b
Angela Porcelli <77314532+aporcelli-LIS@users.noreply.github.com>
2021-03-15 02:06:45 -0400

    Add files via upload
    Failed attempt to create a unit test for a count module.

---
+++ Unittest_Count_Mod9.py
@@ -0,0 +1,11 @@
+from itertools import count
+import unittest
+from unittest import result
+import Mod_8_Infinite
+
+class testCount(unittest.TestCase):
+    def test_Count(self):
+        result=count(4,52)
+        self.assertCountEqual(count, 4,8,12,...)
+if __name__ == '__main__':
+    unittest.main()
